a48b02ee296a36641b17718887bf927718f50066
井上洋孝 <162237779+LxInoue@users.noreply.github.com>
2025-03-28 16:59:52 +0900

    反転削除に伴い不要なコードも削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -2,7 +2,6 @@
 
 import minimist from "minimist";
 import { DateTime } from "luxon";
-import chalk from "chalk";
 
 const today = DateTime.now();
 const args = minimist(process.argv.slice(2));
@@ -27,7 +26,6 @@ console.log("日 月 火 水 木 金 土");
 process.stdout.write("   ".repeat(firstDay.weekday % 7));
 
 for (let day = 1; day <= lastDay.day; day++) {
-  const currentDay = DateTime.local(year, month, day);
   let dayString = day.toString().padStart(2, " ");
 
   if ((firstDay.weekday + day - 1) % 7 === 6) {
